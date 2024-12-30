b245408c59680b8c1c7c4d2213ca764978a7cae6
Selene Moonsilver <bobdablob42+gamedev@gmail.com>
2024-12-30 16:58:32 -0500

    Reset cell visited after making maze, add tests

--- tests.py
+++ tests.py
@@ -24,6 +24,14 @@ class Tests(unittest.TestCase):
         self.assertEqual(m1._cells[0][0].has_top_wall, False)
         self.assertEqual(m1._cells[num_cols - 1][num_rows - 1].has_bottom_wall, False)
 
+    def test_reset_visited(self):
+        num_cols = 12
+        num_rows = 10
+        m1 = Maze(0, 0, num_rows, num_cols, 10, 10)
+        self.assertEqual(m1._cells[0][0].visited, False)
+        self.assertEqual(m1._cells[3][2].visited, False)
+        self.assertEqual(m1._cells[num_cols - 1][num_rows - 1].visited, False)
+
 
 if __name__ == "__main__":
     unittest.main()
