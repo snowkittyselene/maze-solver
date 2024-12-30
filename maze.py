dd1cdb2dc817b4791c5ee6c6f841fb9d41ad4319
Selene Moonsilver <bobdablob42+gamedev@gmail.com>
2024-12-30 15:46:17 -0500

    Add maze class

---
+++ maze.py
@@ -0,0 +1,54 @@
+from cell import Cell
+import time
+
+
+class Maze:
+    def __init__(
+        self,
+        x1,
+        y1,
+        num_rows,
+        num_cols,
+        cell_size_x,
+        cell_size_y,
+        win,
+    ):
+        self._x1 = x1
+        self._y1 = y1
+        self._num_rows = num_rows
+        self._num_cols = num_cols
+        self._cell_size_x = cell_size_x
+        self._cell_size_y = cell_size_y
+        self._win = win
+        self._create_cells()
+
+    def _create_cells(self):
+        # Create the cells
+        self._cells = []
+        for i in range(self._num_cols):
+            column = []
+            for j in range(self._num_rows):
+                column.append(Cell(self._win))
+            self._cells.append(column)
+
+        # Draw the cells
+        for i in range(self._num_cols):
+            for j in range(self._num_rows):
+                self._draw_cell(i, j)
+
+    def _draw_cell(self, i, j):
+        # Get cell size
+        cell_x1 = self._x1 + (i * self._cell_size_x)
+        cell_x2 = cell_x1 + self._cell_size_x
+        cell_y1 = self._y1 + (j * self._cell_size_y)
+        cell_y2 = cell_y1 + self._cell_size_y
+
+        # Draw cell
+        self._cells[i][j].draw(cell_x1, cell_y1, cell_x2, cell_y2)
+        self._animate()
+
+    def _animate(self):
+        if self._win is None:
+            return
+        self._win.redraw()
+        time.sleep(0.05)
