0110a09d8c2d6417d2f7fcd21341fd90a13808eb
Selene Moonsilver <bobdablob42+gamedev@gmail.com>
2024-12-30 14:45:27 -0500

    Add ability to draw lines

--- graphics.py
+++ graphics.py
@@ -22,3 +22,29 @@ class Window:
 
     def close(self):
         self.__running = False
+
+    def draw(self, line, fill_color):
+        line.draw(self.__canvas, fill_color)
+
+
+class Point:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+
+class Line:
+    def __init__(self, point_one, point_two):
+        self.point_one = point_one
+        self.point_two = point_two
+
+    # Takes a Canvas (from tkinter) and fill colour, fill colour being e.g. "black", "red"
+    def draw(self, canvas, fill_color):
+        canvas.create_line(
+            self.point_one.x,
+            self.point_one.y,
+            self.point_two.x,
+            self.point_two.y,
+            fill=fill_color,
+            width=2,
+        )
